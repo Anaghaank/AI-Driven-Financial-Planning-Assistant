e8ec8c1575f2a2e3a7c614cedc1243c1b255ed70
Anaghaank <anaghaankolekar@gmail.com>
2025-10-31 14:02:29 +0530

    Added rs instead of dollar

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -220,7 +220,7 @@ export default function Dashboard() {
                   cx="50%"
                   cy="50%"
                   labelLine={false}
-                  label={(entry) => `${entry.name}: $${entry.value}`}
+                  label={(entry) => `${entry.name}: ₹${entry.value}`}
                   outerRadius={80}
                   fill="#8884d8"
                   dataKey="value"
